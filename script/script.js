7081049957a814cec8941d903a5762d8b206f970
Patrick Q <pquach3@my.bcit.ca>
2023-04-04 14:14:01 -0700

    Added functionality into New Note buttton where it unhides textbox/save button/cancel button and clears the text if not hidden

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ let newBtn = document.querySelector(".box1")
 let darkBtn = document.querySelector(".purple")
 let saveBtn = document.querySelector(".save")
 let cancelBtn = document.querySelector(".red")
+let textBox = document.querySelector('.text');
+
 function darkMode() {
     let elementAside = document.querySelector("aside");
     let elementTextBody = document.querySelector(".text");
@@ -15,11 +17,23 @@ function darkMode() {
         darkBtn.textContent = "Dark Theme";
     }
 };
+
 function cancel() {
-    let textBox = document.querySelector('.text');
     textBox.classList.add("hidden");
     saveBtn.classList.add("hidden");
     cancelBtn.classList.add("hidden");
 };
+
+function newNote() {
+    if (textBox.classList.contains("hidden")) {
+        textBox.classList.remove("hidden");
+        saveBtn.classList.remove("hidden");
+        cancelBtn.classList.remove("hidden");
+    }
+    else {
+        textBox.value = ""
+    }
+}
 darkBtn.addEventListener('click', darkMode);
 cancelBtn.addEventListener('click', cancel);
+newBtn.addEventListener('click', newNote);
